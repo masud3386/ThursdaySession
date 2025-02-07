d5c9bef7e0bcac2b36376e35fc26a36e135c99ff
masud3386 <masud3386@yahoo.com>
2025-02-06 20:54:52 -0500

    this is my first commit to you guys

---
+++ src/test/java/lessonOverview/Cloneingproject.java
@@ -0,0 +1,16 @@
+package lessonOverview;
+
+public class Cloneingproject {
+
+	public static void main(String[] args) {
+/*
+		Hello guys, How are you guys doing 
+		. Clone the project in to your ecpiple
+		and create a class, then make 
+		some modification and  push for pull request.
+		
+		*/
+		
+	}
+
+}
