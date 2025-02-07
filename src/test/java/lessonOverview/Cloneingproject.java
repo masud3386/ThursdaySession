2a5633835f22c2fb0f09372ad18f7aed5ded18d7
Faiha123 <fmhoque@gmail.com>
2025-02-06 21:58:26 -0500

    Commit message from Faiha.

--- src/test/java/lessonOverview/Cloneingproject.java
+++ src/test/java/lessonOverview/Cloneingproject.java
@@ -5,7 +5,7 @@ public class Cloneingproject {
 	public static void main(String[] args) {
 /*
 		Hello guys, How are you guys doing 
-		. Clone the project in to your ecpiple
+		. Clone the project in to your eclipse
 		and create a class, then make 
 		some modification and  push for pull request.
 		
---
+++ src/test/java/lessonOverview/Faiha.java
@@ -0,0 +1,11 @@
+package lessonOverview;
+
+public class Faiha {
+
+	public static void main(String[] args) {
+		// This class is created by Faiha. Cloned the ThursdaySession Project.
+		// For Clone, push and pull practice.
+
+	}
+
+}
