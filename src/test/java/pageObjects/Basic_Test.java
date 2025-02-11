e7378a975e911fbab1361b0fba9ee417f69d5dda
bdblacklist <motiurny@gmail.com>
2025-02-11 18:21:09 -0500

    hello change

---
+++ src/test/java/pageObjects/Basic_Test.java
@@ -0,0 +1,28 @@
+package pageObjects;
+
+public class Basic_Test {
+
+	public static void main(String[] args) {
+		/*
+		  
+		 
+		int a = 1;
+		int b = 5;
+		System.out.println(a+b);
+		
+		*/
+		int a = 1;
+		int b = 2;
+		int c = 3;
+		int d = 4;
+		int e = 5;
+		int f = 6;
+		
+		//System.out.println(a+b+c+d+e+f);
+		//System.out.println(d * f);
+		//System.out.println(f / b);
+		System.out.println(f-e);
+		
+	}
+
+}
