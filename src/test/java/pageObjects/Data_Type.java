ecccbbe2f11e438ab48938356fb0d0f0cd576553
bdblacklist <motiurny@gmail.com>
2025-02-11 18:30:53 -0500

    new commits for test

---
+++ src/test/java/pageObjects/Data_Type.java
@@ -0,0 +1,18 @@
+package pageObjects;
+
+public class Data_Type {
+	
+	int x = 1;
+
+	public static void main(String[] args) {
+		
+		Data_Type myObj = new Data_Type();
+		System.out.println(myObj.x);
+		
+		
+		
+		
+
+	}
+
+}
